067a3cc1dd2d92a746c30b8864e104d0f0f58e26
sy2729 <sy2729@tc.columbia.edu>
2018-05-23 20:22:59 -0400

    init/add basic click slide and next/prev slde

---
+++ app.js
@@ -0,0 +1,86 @@
+let length = $('.carousel').children().length;
+for(let i = 0; i < length; i++) {
+    var button = document.createElement(`div`);
+    button.className = `btn${i}`;
+    $('.buttonWrap').append(button);
+}
+
+var first = $('.carousel').children().eq(0).clone(true)[0];
+var last = $('.carousel').children().eq(length-1).clone(true)[0];
+
+$('.carousel').append(first)
+$('.carousel').prepend(last)
+
+let current = 0;
+$('.carousel').css({
+	transform: 'translateX(-530px)'
+});
+
+$('.buttonWrap').on('click', 'div', function(e) {
+	let button = e.currentTarget;
+	let index = $(button).index();
+	goToSlide(index)
+})
+
+$('#prev').on('click', function() {
+	goToSlide(current - 1)
+})
+
+$('#next').on('click', function() {
+	goToSlide(current + 1)
+})
+
+
+
+function goToSlide(index) {
+
+	if(index > length - 1) {
+		index = 0;
+	}else if (index < 0) {
+		index = length - 1;
+	}
+	console.log(index)
+
+	if(current === length - 1 && index === 0) {
+
+		console.log('special was trriggered')
+		$('.carousel').css({
+			transform:`translateX(${-(length + 1) * 530}px)`
+		}).one('transitionend', function() {
+			$('.carousel').css({
+				transform:`translateX(${-(index + 1) * 530}px)`
+			}).hide().offset();
+			$('.carousel').show();
+		})
+
+	}else if(current === 0 && index === length - 1) {
+
+		console.log('another case')
+		$('.carousel').css({
+			transform:`translateX(${0}px)`
+		}).one('transitionend', function() {
+			$('.carousel').css({
+				transform:`translateX(${-(index + 1) * 530}px)`
+			}).hide().offset();
+			$('.carousel').show();
+		})
+
+	}else{
+		$('.carousel').css({
+			transform:`translateX(${-(index + 1) * 530}px)`
+		})
+	}
+	current = index;
+}
+
+
+
+
+// let timer = setInterval(function() {
+
+// 	$('carousel').css({
+// 		transform: `translateX(${-(index * 400)}px)`
+// 	})
+
+// 	console.log('haha')
+// }, 2000)
